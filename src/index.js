21adfb0e94a2f91d03f43f176683a73e3e78224f
Strand Hornnes <mette.strand-hornnes@capgemini.com>
2020-10-06 16:28:12 +0200

    Started new project and deleted files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
+// import the React and ReactDOM libraries
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactDOM from 'react-dom'
 
+function getButtonText(name) {
+    return "Click here! " + name;
+}
+
+// Create a react component
+//const App = () =>{
+const App = function(){
+
+    // const buttonText = 'Click Me!';
+    //const array = [1,"Hei"];
+    const object = {name: "Oscar", age: 55};
+
+    return ( 
+        <div>
+            <label className="label" htmlFor="name">Enter name:</label>
+            <input id="name" type="text" defaultValue={object.name} />
+            <button style={{backgroundColor: 'red', color: 'white'}}>
+                {getButtonText("Mette")}
+            </button>
+        </div>
+    )
+};
+
+
+// Take the react component and show it on the screen
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    // Komponenten man ønsker fremvist
+    <App />,
+    // Referanse til DOM-elementet i html-filen hvor den skal fremvises.
+    document.querySelector('#root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
