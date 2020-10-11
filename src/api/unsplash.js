d8ad763582cad52973e309f567a751669e5fa773
Mette <mette-strand@live.no>
2020-10-11 20:41:42 +0200

    sectionSeven done

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -1,1 +1,8 @@
 import axios from 'axios';
+
+export default axios.create({
+    baseURL: 'https://api.unsplash.com',
+    headers: {
+        Authorization: 'Client-ID zdDfllJi1Ej20Wd7pSk_jz5Zd8paaLtyxd_lsqrfYMc' 
+    }
+});
--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import axios from 'axios';
+import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar';
 
 
@@ -12,18 +12,8 @@ class App extends React.Component {
     onSearchSubmit = async (term) => {
         // this = onSubmit. 
         // term vilnå inneholder søkeordet fra SearchBar (state.term)
-
-        // Asyncronous request
-        // Det vil ta noe tid fra requesten sendes til man får responsen. 
-        // axios returnerer alltid en respons kalt promise. En promise er et objekt som vil gi oss en notification når noe jobb, f.eks netverks request, er ferdig. 
-
-        const response = await axios.get('https://api.unsplash.com/search/photos', {
-            params: {
-                query: term
-            },
-            headers: {
-                Authorization: 'Client-ID zdDfllJi1Ej20Wd7pSk_jz5Zd8paaLtyxd_lsqrfYMc' 
-            }
+        const response = await unsplash.get('/search/photos', {
+            params: { query: term },   
         });
 
         this.setState({ images: response.data.results });
